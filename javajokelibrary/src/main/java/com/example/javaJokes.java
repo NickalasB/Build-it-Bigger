d732b25aae63b0920cdc02b524bbc90e232e1dd6
Nick Bradshaw <nickalasb@gmail.com>
2016-09-17 09:06:22 -0700

    WORKING made it so button returns random string punchline

--- javajokelibrary/src/main/java/com/example/javaJokes.java
+++ javajokelibrary/src/main/java/com/example/javaJokes.java
@@ -1,8 +1,34 @@
-package com.example;
+package com.udacity.gradle.builditbigger;
+
+
+import java.util.Random;
+
+
+import java.util.Random;
 
 public class javaJokes {
+    public Random mRandom = new Random();
+    String randomPunchLine;
+
+
     public String getJavaJoke1() {
-        return "A penguin wearing sunglasses!";
+        String[] jokeList = new String[]{
+                "A penguin wearing sunglasses!",
+                "Corgis!",
+                "Turtles that wear pants!",
+                "Chocolate covered olives!",
+                "Steve Gutenberg!",
+                "He-Man!",
+                "Dudes named Floyd!"};
+        int INDEXn = mRandom.nextInt(jokeList.length);
+        for (int i2 = 0; i2 < INDEXn; i2++) {
+            randomPunchLine = (String)(jokeList[INDEXn]);
+
+        }
+
+        return randomPunchLine;
     }
 
 }
+
+
